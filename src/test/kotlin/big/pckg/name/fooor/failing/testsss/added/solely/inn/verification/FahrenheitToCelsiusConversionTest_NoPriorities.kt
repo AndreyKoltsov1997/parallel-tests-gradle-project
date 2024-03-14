d1391cc2bcd253d3149bd0e5779bce1399a00bce
Andrey.Koltsov <andrey.koltsov@jetbrains.com>
2024-03-14 19:29:17 +0100

    Add failing test w/o priorities.

---
+++ src/test/kotlin/big/pckg/name/fooor/failing/testsss/added/solely/inn/verification/FahrenheitToCelsiusConversionTest_NoPriorities.kt
@@ -0,0 +1,26 @@
+package big.pckg.name.fooor.failing.testsss.added.solely.inn.verification
+
+import com.jetbrains.converters.big.pckg.a.really.looong.naaaaame.fooooor.teeeests.FahrenheitToCelsiusConverter
+import org.testng.Assert.assertEquals
+import org.testng.annotations.DataProvider
+import org.testng.annotations.Test
+
+class FahrenheitToCelsiusConversionTest_NoPriorities {
+    @Test(dataProvider = "conversionDataFahrenheitToCelsius")
+    fun testFahrenheitToCelsiusConversion(fahrenheit: Double, expectedCelsius: Double) {
+        val result = FahrenheitToCelsiusConverter.convert(fahrenheit)
+        assertEquals(result, expectedCelsius, 0.001, "Conversion from Fahrenheit to Celsius is incorrect")
+    }
+
+    @DataProvider(name = "conversionDataFahrenheitToCelsius")
+    fun conversionDataFahrenheitToCelsius(): Array<Array<Double>> {
+        return arrayOf(
+            arrayOf(32.0, 0.0),
+            arrayOf(212.0, 100.0),
+            arrayOf(-40.0, -40.0),
+            arrayOf(98.6, 37.0),
+            // will fail
+            arrayOf(-1111.0, -1111.0)
+        )
+    }
+}
